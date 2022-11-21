362a663910b212ffefd7da4b9bf8de65075f7d1f
santiagomainardi <smainardi@itba.edu.ar>
2022-11-21 19:06:17 -0300

    Optimización y ultimando detalles
    Le saqué el Sortable.js, borré unas líneas de código repetidas del css y comenté el script.js donde era necesario.

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-// Info date
+// Info Fecha
 const dateNumber = document.getElementById('dateNumber');
 const dateText = document.getElementById('dateText');
 const dateMonth = document.getElementById('dateMonth');
 const dateYear = document.getElementById('dateYear');
-
-// Tasks Container
-const tasksContainer = document.getElementById('tasksContainer');
 
 const setDate = () => {
     const date = new Date();
@@ -15,45 +12,41 @@ const setDate = () => {
     dateYear.textContent = date.toLocaleString('es', { year: 'numeric' });
 };
 
+const tasksContainer = document.getElementById('tasksContainer');
+
 const addNewTask = event => {
     event.preventDefault();
-    const { value } = event.target.taskText; //Agarra los datos del formulario
+    const { value } = event.target.taskText;
     if(!value) return; // Evita que se agreguen tareas sin texto
-    const task = document.createElement('div');
-    task.classList.add('task', 'roundBorder'); // define ese div como task y roundborder
-    task.addEventListener('click', changeTaskState) //cuando se hace click en la tarea, recibimos un evento
+    const task = document.createElement('div'); 
+    task.classList.add('task', 'roundBorder');
+    task.addEventListener('click', changeTaskState) //para cambiar de tasks por hacer a hechas ("done")
     task.textContent = value;
-    //para borrar tareas
-    const borrar = document.createElement('div');
+    const borrar = document.createElement('div'); 
     borrar.className = 'borrar'
     borrar.innerHTML = "X"
-    task.appendChild(borrar)
-    borrar.addEventListener('click', ()=> {
-        tasksContainer.removeChild(task);
-    })
+    task.appendChild(borrar) // "X" que permite borrar tareas
+    borrar.addEventListener('click', ()=> {tasksContainer.removeChild(task);}) //borra task de la lista
     tasksContainer.prepend(task); //agrega task a la lista
-    event.target.reset();
+    event.target.reset(); 
 };
 
 const changeTaskState = event => {
-    event.target.classList.toggle('done'); //Si la lista de clases no tiene done se la agrega. Si la tiene, se la saca. 
+    event.target.classList.toggle('done'); //toggle: agrega o saca "done" de la lista de clases
 };
 
-const order = () => { //ordenar las tareas
+//ordenar las tareas
+const order = () => {
     const done = []; //tareas hechas
     const toDo = []; //tareas por hacer
-    tasksContainer.childNodes.forEach( el => { //forEach itera cada elemento adentro del task container 
-        el.classList.contains('done') ? done.push(el) : toDo.push(el) //si la lista de clases del el tiene done, agrega el elemento a done, sino, a push. 
+    tasksContainer.childNodes.forEach( elemento => { //forEach itera cada elemento adentro del task container 
+        elemento.classList.contains('done') ? done.push(elemento) : toDo.push(elemento) //si la lista de clases del elemento tiene "done", agrega el elemento a done. Si no, a toDo. 
     })
     return [...toDo, ...done]; //devuelve un array (spread ...) primero del toDo y después done
 }
 
 const renderOrderedTasks = () => {
-    order().forEach(el => tasksContainer.appendChild(el)) //itera el array y lo agrega al taskContainer
+    order().forEach(elemento => tasksContainer.appendChild(elemento)) //itera el array y lo agrega al taskContainer
 }
 
 setDate();
-
-new Sortable(tasksContainer, {
-    animation: 300
-});
